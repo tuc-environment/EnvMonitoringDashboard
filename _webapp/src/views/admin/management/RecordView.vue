1bfcc580aa91a7fb3aa46b9151b1596c3fb87822
Yiwei Gong <imwithye@gmail.com>
2023-08-13 10:33:20 +0000

    add table

--- _webapp/src/views/admin/management/RecordView.vue
+++ _webapp/src/views/admin/management/RecordView.vue
@@ -1,1 +1,13 @@
-<template>Record</template>
+<template>
+  <PagedTableComponent />
+</template>
+
+<script lang="ts">
+import PagedTableComponent from '@/components/PagedTableComponent.vue'
+
+export default {
+  components: {
+    PagedTableComponent
+  }
+}
+</script>
