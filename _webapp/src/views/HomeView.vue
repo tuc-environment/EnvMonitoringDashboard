845efa93522965b947409d3558662cf0f9a12844
Lawrence <lawrence@Lawrences-MacBook-Pro.local>
2023-08-20 00:44:49 +0800

    feat: add home page map

--- _webapp/src/views/HomeView.vue
+++ _webapp/src/views/HomeView.vue
@@ -1,1 +1,7 @@
-<template>Hello World, here shall go with the map</template>
+<template>
+    <MapContainer />
+</template>
+
+<script setup lang="ts">
+import MapContainer from '@/components/MapContainer.vue'
+</script>
