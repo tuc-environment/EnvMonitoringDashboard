af451c8243255cea31caaec8c4a291a6049f1b2b
Yiwei Gong <imwithye@gmail.com>
2023-08-20 08:23:00 +0000

    style: adjust ui of register page

--- _webapp/src/views/RegisterView.vue
+++ _webapp/src/views/RegisterView.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="container">
     <div class="mx-auto my-5 login py-3 px-5">
+      <div>
+        <img class="logo" src="/logo.png" />
+      </div>
+
       <h2 class="my-3 text-center">Register</h2>
 
       <div class="my-3 mb-5 text-center">
@@ -21,22 +25,21 @@
         <div class="form-group my-3">
           <label class="mb-2">Confirm password</label>
           <input type="password" class="form-control" v-model="passwordConfirm" />
-        </div>
-
-        <!-- check password match-->
-        <div v-show="showPasswordMatch" class="msg">
-          <p v-if="passwordMatch" class="text-success">Password match</p>
-          <p v-else class="text-danger">Password not match</p>
+          <!-- check password match-->
+          <div v-show="showPasswordMatch" class="small mt-2">
+            <span v-if="passwordMatch" class="text-success">Password match</span>
+            <span v-else class="text-danger">Password not match</span>
+          </div>
         </div>
 
         <button
           type="submit"
           class="mt-3 w-100 btn btn-lg btn-primary"
-          :disabled="requesting"
+          :disabled="requesting || !passwordMatch"
           @click="onSubmit"
         >
           <!-- Register -->
-          <div v-if="requesting" class="spinner-border text-light" role="status" />
+          <div v-if="requesting" class="spinner-border text-light mx-auto d-block" role="status" />
           <div v-else>Register</div>
         </button>
 
@@ -44,9 +47,9 @@
           class="mt-5 d-flex justify-content-between mb-2"
           style="color: rgba(0, 0, 0, 0.5); font-size: 0.99rem"
         >
-          <div>Privacy Policy</div>
-          <div>Term of Service</div>
-          <div>Contact</div>
+          <div style="cursor: pointer">Privacy Policy</div>
+          <div style="cursor: pointer">Term of Service</div>
+          <div style="cursor: pointer">Contact</div>
         </div>
       </form>
     </div>
