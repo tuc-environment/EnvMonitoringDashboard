ef3bc4a1fe8fac126dcfda26f56462d43ae20971
Lawrence <lawrence@Lawrences-MacBook-Pro.local>
2023-08-27 13:00:43 +0800

    feat(fe): add toast to guide users to use prediction feature

--- _webapp/src/main.ts
+++ _webapp/src/main.ts
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import './assets/main.css'
+import Vue3Toastify, { type ToastContainerOptions } from 'vue3-toastify'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -16,5 +17,8 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(VueApexCharts)
+app.use(Vue3Toastify, {
+  autoClose: 3000
+} as ToastContainerOptions)
 
 app.mount('#app')
