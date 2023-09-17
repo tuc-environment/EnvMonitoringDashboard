4fbee5f73a5aa327a7a2f5ae120b778105a29ea1
Yiwei Gong <imwithye@gmail.com>
2023-09-17 21:15:59 +0800

    update model

--- model/model.py
+++ model/model.py
@@ -1,27 +1,17 @@
+import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
-import csv
 
-TRAIN_DATA_PATH = "./model/train.csv"
-LABEL_DATA_PATH = "./model/label.csv"
-
-
-def read_csv(path):
-    with open(path, "r") as f:
-        reader = csv.reader(f)
-        data = list(reader)
-        data = np.array(data, dtype="float64")
-    return data
-
+TRAIN_DATA_PATH = "./model/train.xlsx"
 
 class Model:
     def __init__(self) -> None:
-        pattern = read_csv(TRAIN_DATA_PATH)
-        label = read_csv(LABEL_DATA_PATH)
+        train = pd.read_excel(TRAIN_DATA_PATH, "Sheet1")[2:].to_numpy()
+        label = pd.read_excel(TRAIN_DATA_PATH, "Sheet2")[2:].to_numpy()
 
         self.clf = RandomForestRegressor(random_state=3)
-        self.clf.fit(pattern, label.ravel())
+        self.clf.fit(train, label)
         print("Model is initialized.")
 
     def predict(self, args: np.ndarray) -> np.ndarray:
-        return self.clf.predict(args)
+        return self.clf.predict(args).ravel()
